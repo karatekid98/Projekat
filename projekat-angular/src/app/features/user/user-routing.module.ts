90c632ea5ee9cf382add37662ef135a341f2b596
Marija Matic <smatic1998@gmail.com>
2021-03-26 22:35:17 +0100

    added user routing module for lazy loading user component

---
+++ projekat-angular/src/app/features/user/user-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserEditComponent } from '../user/user-edit/user-edit.component';
+import { UserAddComponent } from '../user/user-table/user-add/user-add.component';
+import { UserComponent } from './user.component';
+import { UserTableComponent } from './user-table/user-table.component';
+
+const routes: Routes = [
+    {path: '', component: UserComponent, children: [
+      { path: '', component: UserTableComponent },
+      { path: 'add-user', component: UserAddComponent },
+      { path: 'edit-user/:id', component: UserEditComponent },
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
--- projekat-angular/src/app/features/user/user.module.ts
+++ projekat-angular/src/app/features/user/user.module.ts
@@ -5,16 +5,20 @@ import { SharedModule } from '../../shared/shared.module';
 import { CoreModule } from '../../core/modules/CoreModule';
 import { UserTableComponent } from './user-table/user-table.component';
 import { UserService } from '../../core/services/user-service/user.service';
-import { AdminHomePageModule } from '../admin-home-page/admin-home-page.module';
 import { UserDeleteModalComponent } from './user-table/user-delete-modal/user-delete-modal.component';
 import { UserAddComponent } from './user-table/user-add/user-add.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { RouterModule } from '@angular/router';
+import { UserRoutingModule } from './user-routing.module';
 
 @NgModule({
-  declarations: [UserComponent, UserTableComponent, UserDeleteModalComponent, UserAddComponent],
+  declarations: [UserComponent, UserTableComponent, UserDeleteModalComponent, UserAddComponent, UserEditComponent],
   imports: [
     CommonModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    RouterModule,
+    UserRoutingModule
   ],
   exports: [UserComponent, UserTableComponent, UserAddComponent],
   providers: [UserService]
