0011ec59dc66a7331886b7c6aecfee1a6f731114
Marija Matic <smatic1998@gmail.com>
2021-03-27 22:47:54 +0100

    added routes to all components inside admin page routing module

--- projekat-angular/src/app/features/admin-home-page/admin-page-routing.module.ts
+++ projekat-angular/src/app/features/admin-home-page/admin-page-routing.module.ts
@@ -2,9 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AddressComponent } from '../address/address.component';
+import { UserComponent } from '../user/user.component';
+import { CustomerComponent } from '../customer/customer.component';
+import { InvoiceComponent } from '../invoice/invoice.component';
+import { ProductComponent } from '../product/product.component';
+import { AdminHomePageComponent } from './admin-home-page.component';
 
 const routes: Routes = [
-  { path: '', component: AddressComponent}
+    {path: 'admin-home-page', component: AdminHomePageComponent, children: [
+      { path: '', component: AddressComponent },
+      { path: 'address', component: AddressComponent },
+      { path: 'user', component: UserComponent,  },
+      { path: 'invoice', component: InvoiceComponent },
+      { path: 'product', component: ProductComponent },
+      { path: 'customer', component: CustomerComponent },
+      { path: 'user',
+      loadChildren: () => import('../user/user-routing.module').then(m => m.UserRoutingModule)},
+
+    ],
+  },
+
 ];
 
 @NgModule({
