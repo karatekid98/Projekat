39274921ce4e41f3b859bd0889b492d7592399e9
Marija Matic <smatic1998@gmail.com>
2021-04-03 23:47:43 +0200

    added user data on user card

--- projekat-angular/src/app/features/user-profile/user-profile.component.ts
+++ projekat-angular/src/app/features/user-profile/user-profile.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit, AfterContentInit   {
+  user: User;
 
   constructor() { }
 
+  // TODO: fix NG0100 expression error
   ngOnInit(): void {
+    this.getUser();
   }
+
+  ngAfterContentInit(): void {
+
+
+  }
+  getUser(): void {
+    this.user = JSON.parse(localStorage.getItem('userObject'));
+  }
+
+
+
 
 }
