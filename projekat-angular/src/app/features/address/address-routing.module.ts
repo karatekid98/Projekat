f6e9a46d33beb292b6b11e553377492b45270f40
Marija Matic <smatic1998@gmail.com>
2021-07-16 13:46:42 +0200

    added routes to address component and its children

---
+++ projekat-angular/src/app/features/address/address-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AddressComponent } from './address.component';
+import { AddressTableComponent } from './address-table/address-table.component';
+import { AddressAddComponent } from './address-table/address-add/address-add.component';
+import { AddressEditComponent } from './address-edit/address-edit.component';
+
+
+const routes: Routes = [
+  {path: '', component: AddressComponent, children: [
+      { path: '', component: AddressTableComponent },
+      { path: 'add-address', component: AddressAddComponent },
+      { path: 'edit-address/:id', component: AddressEditComponent },
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule]
+})
+export class AddressRoutingModule { }
---
+++ projekat-angular/src/app/features/address/address.module.ts
@@ -0,0 +1,38 @@
+import { AddressRoutingModule } from './address-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { CoreModule } from 'src/app/core/modules/CoreModule';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { UserRoutingModule } from '../user/user-routing.module';
+import { AddressService } from '../../core/services/address-service/address.service';
+import { AddressTableComponent } from './address-table/address-table.component';
+import { AddressEditComponent } from './address-edit/address-edit.component';
+import { AddressAddComponent } from './address-table/address-add/address-add.component';
+import { AddressComponent } from './address.component';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { UserModule } from '../user/user.module';
+
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [AddressTableComponent, AddressEditComponent, AddressAddComponent, AddressComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    CoreModule,
+    RouterModule,
+    AddressRoutingModule,
+    TranslateModule,
+    UserModule
+  ],
+  exports: [AddressComponent, AddressTableComponent, AddressAddComponent],
+  providers: [AddressService]
+})
+export class AddressModule { }
