d45bf87f59408fdfad3673f3b95a7316e506514e
Marija Matic <smatic1998@gmail.com>
2021-07-17 12:06:15 +0200

    created missing add customer interface

---
+++ projekat-angular/src/app/models/customerForm.ts
@@ -0,0 +1,18 @@
+
+export interface CustomerForm {
+  AddressDto: {
+    city: string;
+    country: string;
+    postcode: string;
+    line: string;
+  };
+  CustomerDto: {
+    firstName: string;
+    lastName: string;
+    phone: string;
+    companyNumber: string;
+    gender: string;
+    addressId?: string;
+    email: string;
+  };
+}
