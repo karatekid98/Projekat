72b79f272abc35eceda54e2f8453564423e854cd
Marija Matic <smatic1998@gmail.com>
2021-07-17 12:05:26 +0200

    added routing and customer module

---
+++ projekat-angular/src/app/features/customer/customer-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer.component';
+import { CustomerTableComponent } from './customer-table/customer-table.component';
+import { CustomerAddComponent } from './customer-table/customer-add/customer-add.component';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+
+
+const routes: Routes = [
+  {path: '', component: CustomerComponent, children: [
+      { path: '', component: CustomerTableComponent },
+      { path: 'add-customer', component: CustomerAddComponent },
+      { path: 'edit-customer/:id', component: CustomerEditComponent },
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule]
+})
+export class CustomerRoutingModule { }
---
+++ projekat-angular/src/app/features/customer/customer.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { CustomerRoutingModule } from './customer-routing.module';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { CoreModule } from 'src/app/core/modules/CoreModule';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CustomerService } from '../../core/services/customer-service/customer.service';
+import { CustomerComponent } from './customer.component';
+import { CustomerTableComponent } from './customer-table/customer-table.component';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+import { CustomerAddComponent } from './customer-table/customer-add/customer-add.component';
+import { UserModule } from '../user/user.module';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+
+@NgModule({
+  declarations: [CustomerComponent, CustomerTableComponent, CustomerEditComponent, CustomerAddComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    CoreModule,
+    RouterModule,
+    CustomerRoutingModule,
+    TranslateModule,
+    UserModule
+  ],
+  exports: [CustomerComponent, CustomerTableComponent, CustomerAddComponent], // TODO: Add missing exports and declarations
+  providers: [CustomerService]
+})
+export class CustomerModule { }
